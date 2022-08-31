6ca0be2c0d53c94d7aaa4337ca400b33530248ee
suzannecorda8 <103393836+suzannecorda8@users.noreply.github.com>
2022-08-31 16:47:31 +0530

    Add files via upload

---
+++ guess_number game.py
@@ -0,0 +1,11 @@
+right_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess=int(input('Guess:'))
+    guess_count += 1
+    if guess==right_number:
+       print('You won!')
+       break
+    else:
+       print("It's wrong!")
